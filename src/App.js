8051200a986d807d89a2037f381104b4f143e76c
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-11-13 19:25:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Connector } from 'mqtt-react-hooks';
+import Status from './Status';
+import { Buffer } from 'buffer';
+const host = "server3.sytiqhub.com"
+const mqttPort = '1883';
+
+Buffer.from("anything", "base64");
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <Connector brokerUrl={`mqtt://${host}:${mqttPort}`}>
+        <Status />
+      </Connector>
     </div>
   );
 }
